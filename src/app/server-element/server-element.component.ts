5330992270fa6a0f25addafc502719f11dc84107
sushildangi <dangi.sushil5@gmail.com>
2017-12-11 22:08:03 +0530

    Custom Properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  constructor() { }
+  @Input() element: { type: string, name: string, content: string };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
