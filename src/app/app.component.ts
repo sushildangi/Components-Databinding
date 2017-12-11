c33c176eb10489794cb0e1cfb38b7d5adae86b11
sushildangi <dangi.sushil5@gmail.com>
2017-12-11 22:46:33 +0530

    Component communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test'}];
 
 
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintDate: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintDate.serverName,
+      content: blueprintDate.serverContent
+    });
+  }
+
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,7 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: { type: string, name: string, content: string };
+  @Input('sevElement') element: { type: string, name: string, content: string };
 
   constructor() {
   }
